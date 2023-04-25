b77a3510398e34f17551ea308f29e79dc8d1932b
Nazar <Dumi_sj18@itstepru.onmicrosoft.com>
2023-04-25 09:22:42 +0500

    Добавьте файлы проекта.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HW_24._04.Pages
+{
+    [BindProperties]
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string Login { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+            ViewData["login"] = Login;
+            ViewData["password"] = Password;
+        }
+    }
+}
---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HW_24._04.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
